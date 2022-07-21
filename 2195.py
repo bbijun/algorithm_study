0692ce00e822e64acaa1290a6c29a2115431b0ed
bbijun <k36769@cau.ac.kr>
2022-07-21 13:41:36 +0900

    2195:ver2

--- 2195.py
+++ 2195.py
@@ -1,25 +1,15 @@
 s = input()
 p = input()
 result = 0
-while len(p) > 0:
-    if len(p) == 1:
+while len(p)>0:
+    if len(p)==1:
         result += 1
         break
-    count = 0
-    for i in range(1, len(p)+1):
+    for i in range(1,len(p)+1):
         if p[:i] in s:
-            #if i == len(p)-1:
-            #    p = ''
-            #    result += 1
-            count += 1
             continue
         else:
-            if count == 1:
-                p = p[1:]
-                result += 1
-                break
-            else:
-                p = p[i-1:]
-                result += 1
-                break
+            p = p[i-1:]
+            result += 1
+            break
 print(result)
