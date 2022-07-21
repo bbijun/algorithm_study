40dc72b1a083102a606d3005809412fe306989f5
bbijun <k36769@cau.ac.kr>
2022-07-21 13:45:47 +0900

    solved: 2195

--- 2195.py
+++ 2195.py
@@ -2,14 +2,9 @@ s = input()
 p = input()
 result = 0
 while len(p)>0:
-    if len(p)==1:
-        result += 1
-        break
-    for i in range(1,len(p)+1):
+    for i in range(len(p),-1,-1):
         if p[:i] in s:
-            continue
-        else:
-            p = p[i-1:]
-            result += 1
+            p = p[i:]
+            result+=1
             break
 print(result)
