a4da8b7389abd9f97b2eb6df0cc0d3fcd82f551d
bbijun <k36769@cau.ac.kr>
2022-07-31 18:26:13 +0900

    solved: BJ1012

--- DFS_BFS/BJ1012.py
+++ DFS_BFS/BJ1012.py
@@ -1,17 +1,26 @@
-def dfs(x,y,maze,n,m):
-    if x<=-1 or x>= n or y<=-1 or y>=m:
-        return False
+from collections import deque
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
+def bfs(x,y, maze):
     if maze[x][y] == 0:
         return False
+    queue = deque()
+    queue.append((x,y))
+    maze[x][y] = 0
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx>=n or nx<=-1 or ny>=m or ny<=-1:
+                continue
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = 0
+                queue.append((nx,ny))
+    return True
 
-    if maze[x][y]==1:
-        maze[x][y]=0
-        dfs(x,y+1, maze,n,m)
-        dfs(x+1,y, maze,n,m)
-        dfs(x,y-1, maze,n,m)
-        dfs(x-1,y, maze,n,m)
-        return True
-    return False
+
 results = []
 q = int(input())
 for _ in range(q):
@@ -28,7 +37,8 @@ for _ in range(q):
 
     for i in range(n):
         for j in range(m):
-            if dfs(i, j, maze,n,m):
+            if maze[i][j] == 1:
+                bfs(i,j,maze)
                 result+=1
     results.append(result)
 for item in results:
