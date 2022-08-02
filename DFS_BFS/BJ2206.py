bccfba9784cdb86dd1cb322da4a4a6d5556507a4
bbijun <k36769@cau.ac.kr>
2022-08-02 14:47:35 +0900

    solving: bj2206

--- DFS_BFS/BJ2206.py
+++ DFS_BFS/BJ2206.py
@@ -1,14 +1,8 @@
 from collections import deque
 n, m = map(int, input().split())
 maze = [list(map(int, input())) for _ in range(n)]
-result = 1
-
-#벽을 1이 아닌 -1로 표기
-for i in range(n):
-    for j in range(m):
-        if maze[i][j] == 1:
-            maze[i][j] = -1
-maze[0][0] = 1
+visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
+visited[0][0] = 1
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
@@ -16,41 +10,27 @@ dy = [1, 0, -1, 0]
 
 def bfs(x,y, broken, maze):
     q = deque()
-    q.append((x,y,0))
+    q.append((x,y,False))
     while q:
         x, y, b = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx<= -1 or nx >= n or ny <=-1 or ny >=m:
+            if nx <= -1 or nx >= n or ny <=-1 or ny >=m:
                 continue
             if nx == n-1 and ny == m-1:
-                return maze[x][y] +1
-            if maze[nx][ny] == -1:
-                if b == 0:
-                    for j in range(4):
-                        nnx = nx + dx[i]
-                        nny = ny + dy[i]
-                        if nnx<= -1 or nnx >= n or nny <=-1 or nny >=m:
-                            continue
-                        if nnx == n - 1 and nny == m - 1:
-                            return maze[x][y] + 2
-                        if maze[nnx][nny] == -1:
-                            continue
-                        else:
-                            if maze[nnx][nny] == 0 or maze[nnx][nny] >= maze[nx][ny]:
-                                maze[nnx][nny] = maze[nx][ny] + 2
-                                q.append((nnx,nny,1))
-                else:
-                    continue
-            else:
-                if maze[nx][ny] == 0 or maze[nx][ny] >= maze[x][y]:
-                    maze[nx][ny] = maze[x][y] + 1
-                    q.append((nx, ny, b))
-        print(q)
+                return visited[x][y] +1
+            if maze[nx][ny] == 0 and visited[nx][ny][0] == 0 and b == False: #해당 블럭이 방문된 적이 없는 경우
+                visited[nx][ny][0] = visited[x][y][0] + 1
+                q.append((nx,ny, False))
+            if maze[nx][ny] == 0 and visited[nx][ny][1] == 0 and b == True:
+                visited[nx][ny][1] = visited[x][y][] + 1
+                q.append((nx, ny, True))
+            if maze[nx][ny] == 1 and visited[nx][ny][0] == 0 and b == False: #벽을 만났고 벽을 부신 적이 없는 경우
+                visited[nx][ny][0] = visited[x][y][0] + 1
+                q.append((nx, ny, True))
+
     return -1
 
-print(bfs(0,0,0, maze))
-
-
+print(bfs(0,0,False, maze))
 
