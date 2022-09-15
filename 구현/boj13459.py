6999118f7868a2c04b269b63df5a690998e6b33c
bbijun <k36769@cau.ac.kr>
2022-09-15 19:41:29 +0900

    leave: boj13459

--- 구현/boj13459.py
+++ 구현/boj13459.py
@@ -2,71 +2,82 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-class Ball:
-    def __init__(self, name, x, y, n, m):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.isin = False
-        self.visited = [[False] * m for _ in range(n)]
-    def Roll(self, p, maze):
-        dx = [-1, 1, 0, 0] #Up Down Left Right
-        dy = [0, 0, -1, 1]
-        if self.isin:
-            return
-        while maze[self.x + dx[p]][self.y + dy[p]] == '.':
-            maze[self.x][self.y] = '.'
-            self.x += dx[p]
-            self.y += dy[p]
-        if maze[self.x + dx[p]][self.y + dy[p]] == 'O':
-            self.isin = True
-        if self.isin:
-            maze[self.x][self.y] = '.'
-        else:
-            maze[self.x][self.y] = self.name
-            self.visited[self.x][self.y] = True
-
-
-
+red = []
+blue = []
 n,m = map(int, input().split())
 maze = []
-red = Ball('R',0,0,n,m)
-blue = Ball('B',0,0,n,m)
+result = 0
+dx = [-1, 1, 0, 0] #U D L R
+dy = [0, 0, -1, 1]
+
+def check_in(loc, maze):
+    x = loc[0]
+    y = loc[1]
+    if maze[x][y] == 'O':
+        return True
+    return False
+
+def roll(color, loc, path, maze):
+    x = loc[0]
+    y = loc[1]
+    nx = x + dx[path]
+    ny = y + dy[path]
+    while maze[nx][ny] == '.':
+        x = nx
+        y = ny
+        nx += dx[path]
+        ny += dy[path]
+    if check_in([nx,ny],maze):
+        return True, -1, -1
+    else:
+        return False, x, y
+
 for _ in range(n):
     tmp = input().rstrip()
     if 'R' in tmp:
-        red.x = _
-        red.y = tmp.find('R')
+        red.append(_)
+        red.append(tmp.find('R'))
+        maze[_][tmp.find('R')] = '.'
     if 'B' in tmp:
-        blue.x = _
-        blue.y = tmp.find('B')
+        blue.append(_)
+        blue.append(tmp.find('B'))
+        maze[_][tmp.find('B')] = '.'
     maze.append(list(tmp))
-#red.visited[red.x][red.y] = True
-#blue.visited[blue.x][blue.y] = True
-result = 0
-
+red_visited = [[False] * m for _ in range(n)]
+blue_visited = [[False] * m for _ in range(n)]
 
 q = deque()
-q.append((0, 1))
-q.append((1,1))
-q.append((2,1))
-q.append((3,1))
+for i in range(1,4):
+    q.append((red, blue, i))
+
 while(q):
-    path, count = q.popleft()
-    if count > 10:
-        continue
-    red.Roll(path, maze)
-    blue.Roll(path, maze)
-    if red.isin and not blue.isin:
-        result = 1
-        break
-    blue.Roll(path, maze)
-    red.Roll(path, maze)
-    if blue.isin:
-        continue
-    if not red.visited[red.x][red.y] or not blue.visited[blue.x][blue.y]:
-        for i in range(4):
-            q.append((i, count+1))
+    red, blue, path = q.popleft()
+    red_original = red[:]
+    blue_original = blue[:]
+    maze[red[0]][red[1]] = 'R'
+    maze[blue[0]][blue[1]] = 'B'
+    r1 = roll(red, path, maze)
+    if r1[0]: #Red가 구멍에 들어간 경우
+        maze[red[0]][red[1]] = '.'
+    else:
+        maze[r1[1]][r1[2]] = 'R'
+        maze[red[0]][red[1]] = '.'
+    b1 = roll(blue, path, maze)
+    if b1[0]:
+        maze[blue[0]][blue[1]] ='.'
+    else:
+        maze[b1[1]][b1[2]] = 'B'
+        maze[blue[0]][blue[1]] = '.'
+
+
+    r2 = roll(r1, path, maze)
+    if r2[0]:
+        maze[r1[0]][r1[0]] = '.'
+    else:
+        maze[r2[1]][r2[2]]= 'R'
+        maze[r1[0]][r1[0]]
+
+
 print(result)
 
 
